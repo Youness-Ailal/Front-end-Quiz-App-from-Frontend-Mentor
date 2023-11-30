ecbb7e8fb5a5cb7073627dde7d94d96091d8a03e
Youness-Ailal <itsyounessaylal@gmail.com>
2023-11-30 19:15:54 +0100

    init-comit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
-// /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx,css,html,scss}"],
-  theme: {
-    extend: {},
-  },
+  output: "./dist/index.css",
 };
